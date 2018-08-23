0f0e6a155e61b28ac1b412035fdddf2d4b281fd7
razorapple <35845382+RazorApple@users.noreply.github.com>
2018-08-24 00:21:11 +0300

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -7,7 +7,7 @@ totalSeconds %= 3600;
 let minutes = Math.floor(totalSeconds / 60);
 let seconds = totalSeconds % 60;
 let uptime = `${hours} hours, ${minutes} minutes and ${seconds} seconds`;
- message.channel.send("${uptime}")
+ message.channel.send(${uptime})
 }
 module.exports.help = {
   name: "uptime"
