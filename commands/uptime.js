5f6409ba272dcabf83ea09969c6e9411496093c2
razorapple <35845382+RazorApple@users.noreply.github.com>
2018-08-24 00:06:11 +0300

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
- let totalSeconds = (client.uptime / 1000);
+ let totalSeconds = (bot.uptime / 1000);
 let hours = Math.floor(totalSeconds / 3600);
 totalSeconds %= 3600;
 let minutes = Math.floor(totalSeconds / 60);
@@ -14,7 +14,7 @@ const embed = {
     }
   ]
 };
-channel.send({ embed });
+message.channel.send({embed});
 };
 
 message.author.send({embed})
