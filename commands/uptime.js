73cfad8ddf276f01ff1b791a7802c9365f0a17dd
razorapple <35845382+RazorApple@users.noreply.github.com>
2018-08-24 00:19:12 +0300

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,20 +1,13 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
- let totalSeconds = (bot.uptime / 1000);
+let totalSeconds = (bot.uptime / 1000);
 let hours = Math.floor(totalSeconds / 3600);
 totalSeconds %= 3600;
 let minutes = Math.floor(totalSeconds / 60);
 let seconds = totalSeconds % 60;
-const embed = {
-  "fields": [
-    {
-      "name": "Uptime.",
-      "value": "pBot has been up for ${hours} hours, ${minutes} minutes and ${seconds} seconds."
-    }
-  ]
-};
-message.channel.send({embed});
+let uptime = `${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+ message.channel.send("${uptime}")
 }
 module.exports.help = {
   name: "uptime"
